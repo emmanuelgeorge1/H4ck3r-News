049c8d76908acfa2b70384fad08d38dbb8e9e227
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-08-28 02:10:37 +0530

    Initial Commit

---
+++ H4ck3r News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4ck3r News
+//
+//  Created by Emmanuel George on 28/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
